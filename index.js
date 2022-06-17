f09085d85aaa01a6386f51c825b3cbfb40529cd8
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2022-06-16 21:56:02 -0300

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const sequelize = require('./database/manager');
 const { Op } = require('sequelize');
 const Bill = require('./models/bill');
